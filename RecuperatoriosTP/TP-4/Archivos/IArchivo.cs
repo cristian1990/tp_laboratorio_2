ff5da744c98f824296a22524d52d35516ab1f267
cristian1990 <cristian___777@hotmail.com>
2017-11-23 13:50:58 -0300

    RecuperatoriosTP
    RecuperatoriosTP

---
+++ RecuperatoriosTP/TP-4/Archivos/IArchivo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public interface IArchivo<T>
+    {
+
+        /// <summary>
+        /// Metodo generico para implementarse en la clase derivada.
+        /// </summary>
+        bool Guardar(T datos);
+
+
+        /// <summary>
+        /// Lista generica para implementarse en la clase derivada.
+        /// </summary>
+        bool Leer(out List<T> datos);
+    }
+}
